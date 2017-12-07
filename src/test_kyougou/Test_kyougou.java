3f499c7505c67e5e2f71d87dddfc270b07989f23
祐一 <@home-PC>
2017-12-07 13:12:28 +0900

    こんばんわ追加

--- src/test_kyougou/Test_kyougou.java
+++ src/test_kyougou/Test_kyougou.java
@@ -15,7 +15,8 @@ public class Test_kyougou {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("こんにちは");       
+        System.out.println("こんにちは");
+        System.out.println("こんばんわ");
     }
     
 }
